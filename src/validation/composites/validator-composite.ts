e6873180f8da8a557c3622ccc382e45754e94589
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-22 19:22:49 -0200

    feat: add ValidatorComposite

---
+++ src/validation/composites/validator-composite.ts
@@ -0,0 +1,16 @@
+import { ValidatorInterface } from "../../presentation/protocols";
+
+export class ValidatorComposite implements ValidatorInterface {
+  private validators!: ValidatorInterface[];
+
+  public constructor(validators: ValidatorInterface[]) {
+    this.validators = validators;
+  }
+
+  public validate(request: any): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate(request);
+      if (error) return error;
+    }
+  }
+}
