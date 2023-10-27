0c7c7881d79c3a01037d23197e7e36772c7ce6b1
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-27 00:40:34 -0200

    feat: add react routes

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,9 +2,16 @@ import "./styles/index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { makeCreateUserPageFactory } from "./factories";
-
-const apiUrl = 'http://localhost:3000';
+import { makeLoginPageFactory } from "./factories/pages/login-page-factory";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>{makeCreateUserPageFactory(apiUrl)}</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" Component={makeCreateUserPageFactory} />
+        <Route path="/login" Component={makeLoginPageFactory} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
