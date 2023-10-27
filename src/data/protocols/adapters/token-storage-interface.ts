d0f5f3f39a098a75ec0f456dbd3ab653b9a56b03
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-27 21:46:03 -0200

    feat: implement get token method

--- src/data/protocols/adapters/token-storage-interface.ts
+++ src/data/protocols/adapters/token-storage-interface.ts
@@ -1,3 +1,4 @@
 export interface TokenStorageInterface {
   store(key: string, value: any): Promise<void>;
+  get(key: string): Promise<any>;
 }
--- src/infra/adapters/storage-adapter.ts
+++ src/infra/adapters/storage-adapter.ts
@@ -2,6 +2,14 @@ import { TokenStorageInterface } from "../../data/protocols";
 
 export class StorageAdapter implements TokenStorageInterface {
   public async store(key: string, value: any): Promise<void> {
-    localStorage.setItem(key, JSON.stringify(value));
+    await Promise.resolve(localStorage.setItem(key, JSON.stringify(value)));
+  }
+
+  public async get(key: string): Promise<any> {
+    let value = await Promise.resolve(localStorage.getItem(key));
+    try {
+      value = JSON.parse(value || "");
+    } catch (error) {}
+    return value;
   }
 }
