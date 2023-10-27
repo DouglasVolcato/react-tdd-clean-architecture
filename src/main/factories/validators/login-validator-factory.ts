8103d1161c3968a9f7e006f85b0c0256d8fd2e78
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-27 00:40:48 -0200

    feat: add LoginValidator

--- src/main/factories/validators/login-validator-factory.ts
+++ src/main/factories/validators/login-validator-factory.ts
@@ -5,6 +5,7 @@ import { ValidatorComposite } from "../../../validation/composites";
 export function makeLoginValidationFactory(): ValidatorInterface {
   return new ValidatorComposite([
     new ValidatorBuilder().of("email").isRequired(),
+    new ValidatorBuilder().of("email").isEmail(),
     new ValidatorBuilder().of("password").isRequired(),
   ]);
 }
