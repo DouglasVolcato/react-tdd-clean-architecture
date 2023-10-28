7224f803b3343d4652984df9fc2b2d4592b5b9fd
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-28 00:36:55 -0200

    test: ensure DeleteUserPage is working

---
+++ tests/presentation/pages/delete-user-page.spec.tsx
@@ -0,0 +1,70 @@
+import { DeleteUserPage } from "../../../src/presentation/pages";
+import { render, waitFor } from "@testing-library/react";
+import {
+  DeleteUserUseCase,
+  GetUserByTokenUseCase,
+} from "../../../src/domain/protocols";
+import {
+  DeleteUserServiceStub,
+  DomTestHelpers,
+  GetUserByTokenServiceStub,
+} from "../../test-utils";
+import React from "react";
+
+type SutMockTypes = {
+  getUserByTokenService?: GetUserByTokenUseCase.Service;
+  deleteUserService?: DeleteUserUseCase.Service;
+};
+
+const makeSut = (mocks?: SutMockTypes): void => {
+  render(
+    <DeleteUserPage
+      getUserByTokenService={
+        mocks?.getUserByTokenService ?? new GetUserByTokenServiceStub()
+      }
+      deleteUserService={
+        mocks?.deleteUserService ?? new DeleteUserServiceStub()
+      }
+    />
+  );
+};
+
+describe("DeleteUserPage", () => {
+  test("Should not call DeleteUserService on page render", async () => {
+    const mockDeleteUserService = new DeleteUserServiceStub();
+    const deleteUserServiceSpy = jest.spyOn(mockDeleteUserService, "execute");
+    makeSut({ deleteUserService: mockDeleteUserService });
+
+    await waitFor(() => {
+      expect(deleteUserServiceSpy).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  test("Should call DeleteUserService on button click", async () => {
+    const mockDeleteUserService = new DeleteUserServiceStub();
+    const deleteUserServiceSpy = jest.spyOn(mockDeleteUserService, "execute");
+    makeSut({ deleteUserService: mockDeleteUserService });
+
+    await DomTestHelpers.clickButton("deleteuser-button");
+
+    await waitFor(() => {
+      expect(deleteUserServiceSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  test("Should not break the app if DeleteUserService throws", async () => {
+    const mockDeleteUserService = new DeleteUserServiceStub();
+    const deleteUserServiceSpy = jest
+      .spyOn(mockDeleteUserService, "execute")
+      .mockImplementationOnce(() => {
+        throw new Error();
+      });
+    makeSut({ deleteUserService: mockDeleteUserService });
+
+    await DomTestHelpers.clickButton("deleteuser-button");
+
+    await waitFor(() => {
+      expect(deleteUserServiceSpy).toHaveBeenCalled();
+    });
+  });
+});
