b5c12235970eb6c2810b48fcf28e25af33df12f7
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-27 22:12:26 -0200

    test: ensure ClientRequestSenderAdapter is working

--- tests/infra/adapters/client-request-sender-adapter.spec.ts
+++ tests/infra/adapters/client-request-sender-adapter.spec.ts
@@ -1,8 +1,8 @@
-import { ClientRequestSenderAdapter } from "../../../src/infra/adapters/client-request-sender-adapter";
-import { ClientPostRequestSenderInterface } from "../../../src/data/protocols";
+import { ClientRequestSenderAdapter } from "../../../src/infra/adapters";
 import axios from "axios";
 
 const urlLink = "any_url";
+const authToken = "any_token";
 const bodyData = {
   value: "any_value",
 };
@@ -17,10 +17,13 @@ jest.mock("axios", () => ({
   post: jest
     .fn()
     .mockImplementationOnce(async () => Promise.resolve(apiResponse)),
+  get: jest
+    .fn()
+    .mockImplementationOnce(async () => Promise.resolve(apiResponse)),
 }));
 
 type SutTypes = {
-  sut: ClientPostRequestSenderInterface;
+  sut: ClientRequestSenderAdapter;
 };
 
 const makeSut = (): SutTypes => {
@@ -29,33 +32,68 @@ const makeSut = (): SutTypes => {
 };
 
 describe("ClientRequestSenderAdapter", () => {
-  test("Should call axios post with correct values", async () => {
-    const { sut } = makeSut();
-    await sut.post(urlLink, bodyData);
-    const axiosPostCalls = jest.spyOn(axios, "post").mock.calls;
+  describe("post", () => {
+    test("Should call axios post with correct values", async () => {
+      const { sut } = makeSut();
+      await sut.post(urlLink, bodyData);
+      const axiosPostCalls = jest.spyOn(axios, "post").mock.calls;
 
-    expect(axios.post).toHaveBeenCalledTimes(1);
-    expect(axiosPostCalls[0][0]).toBe(urlLink);
-    expect(axiosPostCalls[0][1]).toBe(bodyData);
-    expect(axiosPostCalls[0][2]).toEqual({
-      validateStatus: expect.any(Function),
+      expect(axios.post).toHaveBeenCalledTimes(1);
+      expect(axiosPostCalls[0][0]).toBe(urlLink);
+      expect(axiosPostCalls[0][1]).toBe(bodyData);
+      expect(axiosPostCalls[0][2]).toEqual({
+        validateStatus: expect.any(Function),
+      });
+    });
+
+    test("Should return the correct data from axios post", async () => {
+      const { sut } = makeSut();
+      jest
+        .spyOn(axios, "post")
+        .mockReturnValueOnce(Promise.resolve(apiResponse));
+      const data = await sut.post(urlLink, bodyData);
+
+      expect(data).toEqual(apiResponse.data);
+    });
+
+    test("Should throw if axios post throws", async () => {
+      const { sut } = makeSut();
+      jest.spyOn(axios, "post").mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      expect(async () => await sut.post(urlLink, bodyData)).rejects.toThrow();
     });
   });
 
-  test("Should return the correct data from axios post", async () => {
-    const { sut } = makeSut();
-    jest.spyOn(axios, "post").mockReturnValueOnce(Promise.resolve(apiResponse));
-    const data = await sut.post(urlLink, bodyData);
+  describe("get", () => {
+    test("Should call axios get with correct values", async () => {
+      const { sut } = makeSut();
+      await sut.get(urlLink, authToken);
+      const axiosPostCalls = jest.spyOn(axios, "get").mock.calls;
 
-    expect(data).toEqual(apiResponse.data);
-  });
-
-  test("Should throw if axios post throws", async () => {
-    const { sut } = makeSut();
-    jest.spyOn(axios, "post").mockImplementationOnce(() => {
-      throw new Error();
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(axiosPostCalls[0][0]).toBe(urlLink);
+      expect(axiosPostCalls[0][1]?.headers?.authorization).toBe(authToken);
     });
 
-    expect(async () => await sut.post(urlLink, bodyData)).rejects.toThrow();
+    test("Should return the correct data from axios post", async () => {
+      const { sut } = makeSut();
+      jest
+        .spyOn(axios, "get")
+        .mockReturnValueOnce(Promise.resolve(apiResponse));
+      const data = await sut.get(urlLink, authToken);
+
+      expect(data).toEqual(apiResponse.data);
+    });
+
+    test("Should throw if axios get throws", async () => {
+      const { sut } = makeSut();
+      jest.spyOn(axios, "get").mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      expect(async () => await sut.get(urlLink, authToken)).rejects.toThrow();
+    });
   });
 });
