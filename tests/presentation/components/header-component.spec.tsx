38c68e14c8f704767d340fd01ecbe4cc6f4f21f2
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-30 00:38:15 -0200

    test: ensure HeaderComponent is working

--- tests/presentation/components/header-component.spec.tsx
+++ tests/presentation/components/header-component.spec.tsx
@@ -1,104 +1,46 @@
 import { HeaderComponent } from "../../../src/presentation/components";
 import { GetUserByTokenUseCase } from "../../../src/domain/protocols";
 import { render, waitFor } from "@testing-library/react";
-import {
-  DomTestHelpers,
-  GetUserByTokenServiceStub,
-  makeUserEntity,
-} from "../../test-utils";
-import React from "react";
+import { DomTestHelpers, makeUserEntity } from "../../test-utils";
 import { GlobalContext } from "../../../src/presentation/contexts";
+import React from "react";
 
 type SutMockTypes = {
-  getUserByTokenService?: GetUserByTokenUseCase.Service;
-  onUserLogin?: (loggedUser: GetUserByTokenUseCase.Output) => void;
   getLoggedUser?: () => GetUserByTokenUseCase.Output | null;
 };
 
 const makeSut = (mocks?: SutMockTypes): void => {
-  const onUserLogin = mocks?.onUserLogin ?? ((loggedUser: any) => {});
   const getLoggedUser = mocks?.getLoggedUser ?? (() => makeUserEntity());
   render(
-    <GlobalContext.Provider value={{ onUserLogin, getLoggedUser }}>
-      <HeaderComponent
-        getUserByTokenService={
-          mocks?.getUserByTokenService ?? new GetUserByTokenServiceStub()
-        }
-      />
+    <GlobalContext.Provider
+      value={{ onUserLogin: (loggedUser: any) => {}, getLoggedUser }}
+    >
+      <HeaderComponent />
     </GlobalContext.Provider>
   );
 };
 
 describe("HeaderComponent", () => {
-  test("Should call GetUserByTokenService", async () => {
-    const mockGetUserByTokenService = new GetUserByTokenServiceStub();
-    const getUserByTokenServiceSpy = jest.spyOn(
-      mockGetUserByTokenService,
-      "execute"
-    );
-    makeSut({ getUserByTokenService: mockGetUserByTokenService });
-
-    await waitFor(() => {
-      expect(getUserByTokenServiceSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  test("Should show the logged user name if GetUserByTokenService returns a user", async () => {
-    const mockGetUserByTokenService = new GetUserByTokenServiceStub();
-    jest
-      .spyOn(mockGetUserByTokenService, "execute")
-      .mockReturnValueOnce(Promise.resolve(makeUserEntity()));
-    makeSut({ getUserByTokenService: mockGetUserByTokenService });
+  test("Should show the logged user name if GlobalContext has a user", async () => {
+    const mockGetLoggedUser = jest.fn().mockReturnValueOnce(makeUserEntity());
+    makeSut({ getLoggedUser: mockGetLoggedUser });
 
     await waitFor(() => {
       const loggedUserName = DomTestHelpers.getElementById("logged-user-name");
-
       expect(loggedUserName).toBeTruthy();
       expect(loggedUserName?.innerHTML).toBe(makeUserEntity().name);
+      expect(mockGetLoggedUser).toHaveBeenCalledTimes(1);
+      expect(mockGetLoggedUser).toHaveBeenCalledWith();
     });
   });
 
-  test("Should not show the logged user name if GetUserByTokenService returns an error", async () => {
-    const mockGetUserByTokenService = new GetUserByTokenServiceStub();
-    jest
-      .spyOn(mockGetUserByTokenService, "execute")
-      .mockReturnValueOnce(Promise.resolve(new Error()));
-    makeSut({ getUserByTokenService: mockGetUserByTokenService });
+  test("Should not show the logged user name if GlobalContext has not a user", async () => {
+    const mockGetLoggedUser = jest.fn().mockReturnValueOnce(null);
+    makeSut({ getLoggedUser: mockGetLoggedUser });
 
     await waitFor(() => {
       const loggedUserName = DomTestHelpers.getElementById("logged-user-name");
-
       expect(loggedUserName).toBeNull();
-    });
-  });
-
-  test("Should not show the logged user name if GetUserByTokenService throws", async () => {
-    const mockGetUserByTokenService = new GetUserByTokenServiceStub();
-    jest
-      .spyOn(mockGetUserByTokenService, "execute")
-      .mockImplementationOnce(() => {
-        throw new Error();
-      });
-    makeSut({ getUserByTokenService: mockGetUserByTokenService });
-
-    await waitFor(() => {
-      const loggedUserName = DomTestHelpers.getElementById("logged-user-name");
-
-      expect(loggedUserName).toBeNull();
-    });
-  });
-
-  test("Should set GlobalContext user on user login", async () => {
-    const mockOnUserLogin = jest.fn()
-    const mockGetUserByTokenService = new GetUserByTokenServiceStub();
-    jest
-      .spyOn(mockGetUserByTokenService, "execute")
-      .mockReturnValueOnce(Promise.resolve(makeUserEntity()));
-    makeSut({ getUserByTokenService: mockGetUserByTokenService, onUserLogin: mockOnUserLogin });
-
-    await waitFor(() => {
-      expect(mockOnUserLogin).toHaveBeenCalledTimes(1);
-      expect(mockOnUserLogin).toHaveBeenCalledWith(makeUserEntity());
     });
   });
 });
