09e17f5b4e6e09930cda312de05348054f4c38e2
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-29 22:33:12 -0200

    test: ensure header sets global context user on user login

--- tests/presentation/components/header-component.spec.tsx
+++ tests/presentation/components/header-component.spec.tsx
@@ -7,20 +7,25 @@ import {
   makeUserEntity,
 } from "../../test-utils";
 import React from "react";
+import { GlobalContext } from "../../../src/main/contexts";
 
 type SutMockTypes = {
   getUserByTokenService?: GetUserByTokenUseCase.Service;
+  onUserLogin?: (loggedUser: GetUserByTokenUseCase.Output) => void;
+  getLoggedUser?: () => GetUserByTokenUseCase.Output | null;
 };
 
 const makeSut = (mocks?: SutMockTypes): void => {
+  const onUserLogin = mocks?.onUserLogin ?? ((loggedUser: any) => {});
+  const getLoggedUser = mocks?.getLoggedUser ?? (() => makeUserEntity());
   render(
-    <div>
+    <GlobalContext.Provider value={{ onUserLogin, getLoggedUser }}>
       <HeaderComponent
         getUserByTokenService={
           mocks?.getUserByTokenService ?? new GetUserByTokenServiceStub()
         }
       />
-    </div>
+    </GlobalContext.Provider>
   );
 };
 
@@ -80,6 +85,20 @@ describe("HeaderComponent", () => {
       const loggedUserName = DomTestHelpers.getElementById("logged-user-name");
 
       expect(loggedUserName).toBeNull();
+    });
+  });
+
+  test("Should set GlobalContext user on user login", async () => {
+    const mockOnUserLogin = jest.fn()
+    const mockGetUserByTokenService = new GetUserByTokenServiceStub();
+    jest
+      .spyOn(mockGetUserByTokenService, "execute")
+      .mockReturnValueOnce(Promise.resolve(makeUserEntity()));
+    makeSut({ getUserByTokenService: mockGetUserByTokenService, onUserLogin: mockOnUserLogin });
+
+    await waitFor(() => {
+      expect(mockOnUserLogin).toHaveBeenCalledTimes(1);
+      expect(mockOnUserLogin).toHaveBeenCalledWith(makeUserEntity());
     });
   });
 });
